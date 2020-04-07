b6191068b6ae04afc7b340d59da47bd768425762
MoXiaoxing23187 <2861997688@qq.com>
2020-04-07 22:08:59 +0800

    1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
@@ -6,7 +7,59 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int result = DillString.SummaryString(DillString.CreateList());
+            Console.WriteLine(result);
+        }
+    }
+
+    class DillString
+    {
+        public static List<int> CreateList()
+        {
+            List<int> arryList = new List<int>();
+            int sum = int.Parse(Console.ReadLine());
+            if (sum<=0)
+            {
+                arryList.Add(0);
+            }
+            else
+            {
+                for (int i = 0; i < sum; i++)
+                {
+                    int num = int.Parse(Console.ReadLine());
+                    arryList.Add(num);
+                }
+            }
+            return arryList;
+        }
+
+        public static int SummaryString(List<int> arryList)
+        {
+            int maxsum = 0, sum = 0;
+            bool flag = false;
+            foreach (var item in arryList)
+            {
+                if (item>=0)
+                {
+                    flag = true;
+                }
+            }
+            if (flag == true)
+            {
+                for (int i = 0; i < arryList.Count; i++)
+                {
+                    sum = 0;
+                    for (int k = i; k < arryList.Count; k++)
+                    {
+                        sum += arryList[k];
+                        if (sum > maxsum)
+                        {
+                            maxsum = sum;
+                        }
+                    }
+                }
+            }
+            return maxsum;
         }
     }
 }
